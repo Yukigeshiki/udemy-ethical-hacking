ece2cd382518702696de1c72e3e1be625ad8f6d8
Laurence Sonnenberg <laurencesonnenberg01@gmail.com>
2018-03-05 19:29:46 +0200

    Added run script set_monitor_mode_on_wireless_adapter.sh to loop in jam_router.sh.

--- jam_wireless_router/set_monitor_mode_on_wireless_adapter.sh
+++ jam_wireless_router/set_monitor_mode_on_wireless_adapter.sh
@@ -2,6 +2,8 @@
 
 sudo ifconfig ${wireless_adapter} down 
 
+sudo macchanger -r ${wireless_adapter} | grep "New MAC"
+
 sudo iwconfig ${wireless_adapter} mode monitor
 
 sudo ifconfig ${wireless_adapter} up 
